b2a72b6510ecea808ef3d107f7f7bc9aad1db3a5
tanushreesrivastavs125-code <tanushree.srivastav.s.125@kalvium.community>
2026-07-22 13:18:54 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,32 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const todoRoutes = require("./routes/todo");
-
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
 
 const app = express();
 
 connectDB();
 
-
-const authRoutes = require("./routes/auth");
-
-
-const cors = require("cors");
-
 app.use(cors({
   origin: [
     "http://localhost:5500",
     "https://todo-appf.netlify.app"
-  ],
-  credentials: true
+  ]
 }));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Server Running ");
+  res.send("Server Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
